5eb6b9df12292d339fbf7121bbfe7572c7cff8e1
Matheus <matheusrodrigues1>
2026-07-22 11:25:16 -0300

    feat(hackerrank): solve Java If-Else and Stdin/Stdout II challenges

--- java/hackerrank/easy/0001_java_stdin_and_stdout_i/Solution.java
+++ java/hackerrank/easy/0001_java_stdin_and_stdout_i/Solution.java
@@ -13,7 +13,7 @@ public class Solution {
 
         System.out.println(a);
         System.out.println(b);
-        System.out.print(c);
+        System.out.println(c);
         // Complete this line
         // Complete this line
     }
